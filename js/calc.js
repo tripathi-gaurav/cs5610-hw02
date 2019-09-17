6af8321e69812d85b0b301fa3bd442e810dd71c4
Gaurav Tripathi <gaurav@tripathi.org>
2019-09-17 02:53:54 +0000

    basic state change calculator logic added

--- js/calc.js
+++ js/calc.js
@@ -5,41 +5,73 @@ function(){
 	
 
 	var txt_result = document.getElementById('result');
-
+	txt_result.value = "";
 	var click_zero = function(e){
 		//alert("hello");
 		var op = e.target.value;
-		if( op != '/' && op != '*' && op != '+' && op != '*'){
+		if( op == 'C'){
+			val = "";
+			stack = [];
+			txt_result.value = val;
+		}else if(  op != '/' && op != '*' && op != '+' && op != '-'){
 
-		val += op;
-		//stack.push(e.target.value);
-		//txt_result.value += e.target.value;		
-		//console.log(e.target.value);
-		console.log(val);
+			val += op;
+			txt_result.value += op;
+			console.log("www"  + val);
 		}else{
-			var operand_one = parseFloat(val);
+			
 		
 			if(stack.length == 2){
+				//var top_of_stack = stack[stack.length-1];
+				//if( top_of_stack != op && op != '+' ){
+					//stack[stack.length-1] = op;
+				//}else{
+
+				
+				var operand_one = parseFloat(val);
 				var operator = stack.pop();
 				var operand_two = stack.pop();
 				var result = 0;
+				console.log( operand_one );
+					console.log( operator );
+					console.log( operand_two );
 				if( operator == '+'){
 					result = operand_one + operand_two;
 				}else if( operator == '-'){
-					result = operand_one - operand_two;
+					result = operand_two - operand_one;
+				}else if( operator == "*"){
+					result = operand_one * operand_two;
+				}else if( operator == '/'){
+					result = operand_two / operand_one;
 				}
-				stack.push( result );
-				val = result;	
+					stack = [];
+					val = result;	
+					//txt_result.value = val;
+					stack.push(result);
+					stack.push( op );
+					txt_result.value = stack[0] + "" + stack[1];
+					val = "";
+				//}
 			}else{
-				stack.push( operand_one );
-				stack.push( op );
+				//if( stack.length > 0 ){	
+					var operand_one = parseFloat(val);
+					
+                                	stack.push( operand_one );
+					stack.push( op );
+					txt_result.value += op;
+					val = "";
+				//}
+				
 			}
 			console.log(stack);
 		}
 	}
+	//binding all elements using a for loop
+	//borrowed logic from: https://www.reddit.com/r/learnjavascript/comments/5j5quz/add_eventlistener_to_all_buttons_of_class/
 	document.querySelectorAll("button").forEach(function(elem){
 
 		elem.addEventListener("click", click_zero);
 	});
 
 }, false);
+
